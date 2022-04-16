bffb30258d4b3970ec53185263b8ef8d62e32aa4
yuvi_xyz <78093228+yuvicpp@users.noreply.github.com>
2022-04-16 06:30:23 -0600

    Create primeNum.cpp

---
+++ primeNum.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n)
+{
+    if(n==0 || n==1)
+    {
+        return false;
+    }
+    for(int i=2; i<n; i++)
+    {
+        if(n%i==0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for(int i=0; i<n; i++)
+    {
+        if(isPrime(i))
+        {
+            cout << "Prime No.: " << i << endl;;
+        }
+    }
+    cout << endl;
+}
+
+
+// int main()
+// {
+//     int n,m=0,flag=0;
+//     cin >> n;
+
+//     m = n/2;
+
+//     for(int i=2; i<=m; i++)
+//     {
+//         if(n%i==0)
+//         {
+//             cout << "Not a Prime No." << ": " << n << endl;
+//             flag = 1;
+//             break;
+//         }
+//     }
+//     if(flag==0)
+//     {
+//         cout << "Prime No." << ": " << n << endl;
+//     }
+// }
